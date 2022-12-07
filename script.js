398b0a88d54a72e6f6424db45bc94245c1f377a7
Anshu Kumar <anshukumar@Anshus-MacBook.local>
2022-12-08 01:31:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+console.log("Welcome!")
+let audioElement1 =new Audio('Kesariya - Brahmastra 128 Kbps.mp3');
+let audioElement2 =new Audio('2.mp3');
+let audioElement3 =new Audio('3.mp3');
+let audioElement4 =new Audio('4.mp3');
+let audioElement5 =new Audio('5.mp3');
+let audioElement6 =new Audio('6.mp3');
+function play_audio1()
+{
+    audioElement1.play();
+}
+function pause_audio1()
+{
+    audioElement1.pause();
+}
+function play_audio2()
+{
+    audioElement2.play();
+}
+function pause_audio2()
+{
+    audioElement2.pause();
+}
+function play_audio3()
+{ 
+    audioElement3.play();
+}
+function pause_audio3()
+{
+    audioElement3.pause();
+}
+function play_audio4()
+{   
+    audioElement4.play();
+}
+function pause_audio4()
+{
+    audioElement4.pause();
+}
+function play_audio5()
+{   
+    audioElement5.play();
+}
+function pause_audio5()
+{
+    audioElement5.pause();
+}
+function play_audio6()
+{   
+    audioElement6.play();
+}
+function pause_audio6()
+{
+    audioElement6.pause();
+}
